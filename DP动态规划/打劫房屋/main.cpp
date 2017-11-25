a2079f70474d4b9a67dd7db4853f74dc8218a944
100innovate <gamefy2010@qq.com>
2017-11-25 17:39:59 +0800

    e

--- DP动态规划/打劫房屋/main.cpp
+++ DP动态规划/打劫房屋/main.cpp
@@ -5,33 +5,22 @@
 using namespace std;
 
 class Solution {
-public:
+	public:
 	long long houseRobber(vector<int> &A) {
 		int x = A.size();
 		vector<long long> b;
 		if (x == 0)return 0;
-		if (x == 1)return A[0];
-		for (int i(0); i < x-1; i++) {
+		for (int i(0); i < x; i++) {
 			if (i == 0)b.push_back(A[0]);
 			else if (i == 1)b.push_back(max(A[0], A[1]));
 			else {
 				b.push_back(max(b[i - 1], b[i - 2] + A[i]));
 			}
-		}
-
-		vector<long long> c;
-		c.push_back(0);
-		for (int i(1); i < x; i++) {
-			if (i == 1)c.push_back(A[1]);
-			else if (i == 2)c.push_back(max(A[1], A[2]));
-			else {
-				c.push_back(max(c[i - 1], c[i - 2] + A[i]));
-			}
-		}
-		return max(c[x - 1], b[x - 2]);
-	}
+			
+		}	
+		return b[x - 1];	
+	}	
 };
-
 
 int main() {
 	int x;
