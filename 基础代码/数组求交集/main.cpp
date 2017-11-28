aa1d3616f4f66ff7c056d65b9a50ba42967c04c0
100innovate <gamefy2010@qq.com>
2017-11-28 12:11:41 +0800

    2017.11.28

---
+++ 基础代码/数组求交集/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+class Solution {
+public:
+
+	/*
+	* @param nums1: an integer array
+	* @param nums2: an integer array
+	* @return: an integer array
+	*/
+	vector<int> intersection(vector<int> nums1, vector<int> nums2) {
+		map<int, int>x;
+		vector<int> nums3;
+		for (int i(0); i < nums1.size(); i++)
+			x[nums1[i]] = 1;
+		for (int  i = 0; i < nums2.size(); i++)
+			if (x[nums2[i]]) {
+				nums3.push_back(nums2[i]);
+				x[nums2[i]] = 0;
+			}
+
+		return nums3;
+	}
+};
