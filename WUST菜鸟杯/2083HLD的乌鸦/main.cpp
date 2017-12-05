f846040786613d3296138f3a324cf0259888a14c
100innovate <gamefy2010@qq.com>
2017-12-05 22:21:35 +0800

    change

--- WUST菜鸟杯/2083HLD的乌鸦/main.cpp
+++ WUST菜鸟杯/2083HLD的乌鸦/main.cpp
@@ -6,40 +6,29 @@ using namespace std;
 
 int main() {
 	int m, n;
+	int qqq = 0;
 	while (cin >> m >> n) {
+		qqq++;
 		vector<int> all(n);
-		for (int i(0); i < n; i++)
+		
+		for (int i(0); i < n; i++) {
 			cin >> all[i];
-
-		sort(all.begin(), all.end());
-
- 		int x1 = m;
-		int x2 = 0;
-		int x3 = 0;
-
-		for (int i(n-1); i >=0; i++) {
-			int q = all[i] / 4;
- 			if (x2 >= q) {
-				x2 -= q;
-			}
-			else {
-				x2 -= q;
-				x1 += x2;
-				x2 = 0;
-			}
-
-			int p = all[i] % 4;
-			if (x3 - p < 0) {
-				x1--;
-				x3 += 4 - p;
-				x2++;
-			}
-			else {
-				x3 -= p;
-			}
 		}
-		if (x1 >= 0)cout << "YES" << endl;
-		else cout << "NO" << endl;
+		sort(all.begin(), all.end());
+		int x = all[n-1] / 4;
+		if (all[n-1] % 4)x++;
+		if (x > m) {
+			cout << "Case "<<qqq<<": "<<"NO" << endl;
+			continue;
+		}
+		int sum(0);
+		for (int i(0); i < n; i++)
+			sum += all[i];
+
+		if(sum>(m*8))
+			cout << "Case " << qqq << ": " << "NO" << endl;
+		else cout << "Case " << qqq << ": " << "YES" << endl;
+		
 
 	}
 }
